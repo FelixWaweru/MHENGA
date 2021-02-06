4d79ec5c5cdeb8b5ee40a0af9cfee52b61bae961
unknown <wawerufelix60@gmail.com>
2021-02-06 14:41:38 +0300

    Working version

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
 var Twitter = require('twitter');
-var Swahili = require('swahili-lang');
-
-var swahiliImport = Swahili.impoti("./hesabu.swh")
-
-swahiliImport.PataMethali(1)
  
-// var client = new Twitter({
-//   consumer_key: '',
-//   consumer_secret: '',
-//   access_token_key: '',
-//   access_token_secret: ''
-// });
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
  
-// const spawn = require("child_process").spawn;
-// let options = {shell: true};
-// const pythonProcess = spawn('python',["C:/Users/HP/Documents/Code/GitHub/NodePractice/test.py"],options);
-// const swahiliProcess = spawn('swahili',["C:/Users/HP/Documents/Code/GitHub/NodePractice/mhenga.swh"],options);
-// swahiliProcess.stdin.setEncoding('utf-8');
-// swahiliProcess.stdin.write('1\n');
-// swahiliProcess.stdout.pipe(process.stdout);
-// swahiliProcess.stdin.end();
+function Mhenga(callback)
+{
+	// Node Js spawn function used to run the Mhenga.swh script
+	const spawn = require("child_process").spawn;
+	let options = {shell: true};
+	const swahiliProcess = spawn('swahili',["C:/Users/HP/Documents/Code/GitHub/MHENGA/mhenga.swh"],options);
+	let output = '';
+	// Save the response to a variable
+	swahiliProcess.stdout.on('data', (chunk) => {
+		output += chunk.toString();
+	});
+	swahiliProcess.on('exit', () => {
+		callback(output);
+	});
 
-//Function used to see the specified user timeline
-// var params = {screen_name: 'whyweru'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log(tweets);
-//   }
-// });
+}
 
-// //Function used to post the tweet
-// function tweeter(tweet_post)
-// {
-// 	client.post('statuses/update', {status: tweet_post}, function(error, tweet, response){
-// 			if(!error){
-// 				console.log(tweet);
-// 			}
-// 			else{
-// 				console.log(response);
-// 			}
-// 		})
-// }
+//Function used to post the tweet
+function tweeter(tweet_post)
+{
+	client.post('statuses/update', {status: tweet_post}, function(error, tweet, response){
+			if(error){
+				console.log(tweet_post);
+				console.log("Error Posting Tweet");
+			}
+			else{
+				console.log("Tweet: " + tweet_post + "\n");
+				console.log("Tweet Posted!");
+			}
+		})
+}
+
+// Function execution
+Mhenga(function(output) {
+	tweeter(output)
+	// console.log(output)
+  });
