e26e8a36346a528f3e150747c761e5d676d78bb7
unknown <wawerufelix60@gmail.com>
2021-02-08 18:19:15 +0300

    Added loop for continous posting

--- app.js
+++ app.js
@@ -41,22 +41,61 @@ function tweeter(tweet_post)
 		})
 }
 
-  try {
-	// Function execution
-	Mhenga(function(output) {
-	tweeter(output)
-	// console.log(output)
-  });
-  }
-  catch (e) {
-	  console.log("Trying different tweet")
-	  // Sleep for a while to change the time
-	  setTimeout(Mhenga(function(output)
-	  {
+// function streamer()
+// {
+// 	var stream = client.stream('statuses/filter', {track: '@whyweru methali'});
+// 	stream.on('data', function(event) {
+// 		console.log(event && event.text);
+		
+// 		// Get a methali from the mhenga function
+// 		Mhenga(function(output) {
+// 			client.post('statuses/update', {status: output, in_reply_to_status_id: event.id}, function(error, tweet, response){
+// 				if(error){
+// 					console.log(tweet);
+// 					console.log("Error Posting Tweet");
+// 				}
+// 				else{
+// 					console.log("Tweet: " + output + "\n");
+// 					console.log(tweet);
+// 					console.log("Tweet Posted!");
+// 				}
+// 			})
+// 		});
+// 		  });
+	
+// 	stream.on('error', function(error) {
+// 		throw error;
+// 	});
+// }
+
+
+var hourInterval = 6
+// Post a tweet every 6 - 8 hours
+  setInterval(function () {
+	  //Show the Tweet post time
+	var posttime = new Date();
+	console.log('Posting tweet at ' + posttime + "\n");
+	// Create a random posting time
+	hourInterval = (Math.random() * (8.5 - 6.0) + 6.0).toFixed(1)
+
+	try {
+		// Function execution
+		Mhenga(function(output) {
 		tweeter(output)
 		// console.log(output)
-	  }), 3000);
-  }
-  finally {
-	console.log("Completed");
-  }
+	  });
+	  }
+	  catch (e) {
+		  console.log("Trying different tweet")
+		  // Sleep for a while to change the time
+		  setTimeout(Mhenga(function(output)
+		  {
+			tweeter(output)
+			// console.log(output)
+		  }), 3000);
+	  }
+	  finally {
+		console.log("Completed");
+	  }
+
+ }, hourInterval * 60 * 60 * 1000);
